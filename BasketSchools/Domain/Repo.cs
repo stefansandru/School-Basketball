b56dddf9a046979c37ec3dea5016ecd798527a54
stefansandru <stefansandru04@gmail.com>
2025-01-06 08:20:29 +0200

    ass: delete and update

--- BasketSchools/Domain/Repo.cs
+++ BasketSchools/Domain/Repo.cs
@@ -3,7 +3,8 @@ namespace BasketSchools.Domain;
 public interface Repo
 {
     void Add(Entity e);
-
+    void Delete(int id);
+    void Update(Entity e);
     Entity FindById(int id);
     List<Entity> FindAll();
 }
